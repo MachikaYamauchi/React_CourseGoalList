501fbe5834a455f9ef7fc5a345eded5ef2420f19
Machika Yamauchi <machika111@gmail.com>
2022-09-29 15:57:40 -0700

    Upload CSS modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,35 @@
 import React, { useState } from "react";
+// import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
+
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color:${props => props.invalid ? "red" : "black"};
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => props.invalid ? "red" : "#ccc"};
+//     background-color: ${props => props.invalid ? "rgb(255, 159, 159)" : "transparent"};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -30,9 +58,23 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        {/* If the user entered somethign invalid, we want to add a red border and a slightly red background to the input and also a red color to that label. */}
-        {/* Inline style takes very high priority and override the styles set in  CSS file, so inline style might not be good. */}
+      {/* styles['form-control'] has the same meanig as styles.form-control bacause the styles is object */}
+      {/* To add inline syntax, we use back tick. The first value we want to inject is resolved className.   */}
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`} invalid={!isValid}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+
+      {/* `` is template literal. ${} between thse curly braces can be any Javascript expression.  */}
+      {/* <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div> */}
+
+      {/* If the user entered somethign invalid, we want to add a red border and a slightly red background to the input and also a red color to that label. */}
+      {/* Inline style takes very high priority and override the styles set in  CSS file, so inline style might not be good. */}
+      {/* <div className="form-control">
         <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
         <input
           style={{
@@ -42,8 +84,7 @@ const CourseInput = (props) => {
           type="text"
           onChange={goalInputChangeHandler}
         />
-      </div>
-      <Button type="submit">Add Goal</Button>
+      </div> */}
     </form>
   );
 };
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,43 @@
 import React from 'react';
 
-import './Button.css';
+import styles from './Button.module.css'; // This syntax is for CSS modules
+
+// import styled from 'styled-components';
+// This syuntax is called as tagged template literal. This is a default JavaScript feature. 
+// "button" is simply a method of the "styled" object. to execute the button method, we use `` instead of ();. This button methods return button.
+// We do not have the place where we set up the classes, so the styling will pass between the opening and closing back tick and will directly effect this button. That is why we delete selector in this back tick.
+// The button which is returned all to by default applies all the props we might be passing to out own button component
+// const Button = styled.button`
+//   width:100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+
+//   @media (min-width:768px) {
+//     width:auto;
+//   }
+
+//   // for pseudo selectors, we can use "&"
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    // "className={styles} -> The thing importing from the CSS file turns out to be an object. Every class we defined in the CSS file as a property  "
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
